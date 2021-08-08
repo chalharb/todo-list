6a9b4bc6b47682e113366bc2913c0536f77e8c40
Logan Harber <chalharb@gmail.com>
2021-08-08 13:39:07 -0500

    Created tests for Button Component

---
+++ src/components/button/Button.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import Button from './Button'
+
+test('renders a button element', () => {
+  const { getByRole } = render(
+    <Button>Click me!</Button>
+  );
+
+  expect(getByRole('button')).toBeInTheDocument();
+});
+
+test('rendered button triggers click event', () => {
+  let clicked = false;
+  const handleClick = () => {
+    clicked = true;
+  };
+
+  render(<Button onClick={handleClick}>Click me!</Button>);
+
+  const button = screen.getByRole('button');
+  fireEvent.click(button);
+
+  expect(clicked).toBe(true);
+});
