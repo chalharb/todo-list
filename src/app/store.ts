8bc6b29ccce9751a2707f78374814f6a67fd060e
Logan Harber <chalharb@gmail.com>
2021-08-07 23:40:07 -0500

    Integrated TodoList Component into app

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import todoListReducer from '../features/todo-list/todoListSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    todos: todoListReducer
   },
 });
 
