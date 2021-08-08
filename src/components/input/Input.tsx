9ee6154f107cb4b088ff3113f597cd259bed8a3d
Logan Harber <chalharb@gmail.com>
2021-08-08 14:16:17 -0500

    Created tests for Input Component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import Label from '../label/Label';
 
 interface InputProps {
-  label: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 
 const Input: React.FC<InputProps> = ({
-  label,
   value,
   onChange
 }) => {
 
   return (
     <>
-      <Label labelFor="test">{label}</Label>
       <input type="text" value={value} onChange={onChange}/>
     </>
   );
