0556f09c61206b595412c05348281003f6b18d5b
Logan Harber <chalharb@gmail.com>
2021-08-18 19:55:18 -0500

    Updated tests for TodoListSlice

--- src/features/todolist/todolistSlice.spec.ts
+++ src/features/todolist/todolistSlice.spec.ts
@@ -7,39 +7,63 @@ import todoListReducer, {
 } from './todoListSlice';
 
 describe('TodoList reducer', () => {
+  const initialState = {
+    todoFilter: 'All',
+    todos: [
+      {
+        id: '4d90c13d-54d2-44a9-a474-054a725ad2b6',
+        text: 'Do the laundry',
+        completed: true,
+      },
+    ]
+  };
+
   it('should handle initial state', () => {
-    expect(todoListReducer(undefined, { type: 'unkown' })).toEqual([])
+    expect(todoListReducer(undefined, { type: 'unkown' })).toEqual(initialState)
   });
 
   it('should handle adding a new todo', () => {
-    const initialState: TodoListState = [];
+    const initialState: TodoListState = {
+      todoFilter: 'All',
+      todos: [],
+    };
     const todoText = 'Test Todo';
 
     const actual = todoListReducer(initialState, createTodo(todoText))
-    expect(actual[0].text).toEqual(todoText);
+    expect(actual.todos[0].text).toEqual(todoText);
   });
 
   it('should handle deleting a todo', () => {
     const id = nanoid();
-    const initialState: TodoListState = [{
-      id: id,
-      text: 'Test todo text',
-      completed: false
-    }];
+    const initialState: TodoListState = {
+      todoFilter: 'All',
+      todos: [
+        {
+          id: id,
+          text: 'Test todo text',
+          completed: false
+        }
+      ]
+    };
 
     const actual = todoListReducer(initialState, deleteTodo(id))
-    expect(actual).toEqual([]);
+    expect(actual.todos).toEqual([]);
   });
 
   it('should handle completing a todo', () => {
     const id = nanoid();
-    const initialState: TodoListState = [{
-      id: id,
-      text: 'Test todo text',
-      completed: false
-    }];
+    const initialState: TodoListState = {
+      todoFilter: 'All',
+      todos: [
+        {
+          id: id,
+          text: 'Test todo text',
+          completed: false
+        }
+      ]
+    };
 
     const actual = todoListReducer(initialState, completeTodo(id))
-    expect(actual[0].completed).toEqual(true);
+    expect(actual.todos[0].completed).toEqual(true);
   });
 });
